bb7849dd3ae4276b0974834ee94e5d52f8ad8615
achyutbenz19 <achyut.benz@gmail.com>
2024-05-01 12:12:14 -0500

    components

---
+++ components/model.py
@@ -0,0 +1,45 @@
+import os
+import time
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import LLMChain
+from config.prompt import PROMPT
+
+load_dotenv()
+
+GROQ_API_KEY = os.environ["GROQ_API_KEY"]
+
+class LanguageModelProcessor:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768", groq_api_key=os.getenv("GROQ_API_KEY"))
+        self.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+        system_prompt = prompt
+        
+        self.prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            HumanMessagePromptTemplate.from_template("{text}")
+        ])
+
+        self.conversation = LLMChain(
+            llm=self.llm,
+            prompt=self.prompt,
+            memory=self.memory
+        )
+        
+    def process(self, text):
+        self.memory.chat_memory.add_user_message(text)
+
+        start_time = time.time()
+
+        response = self.conversation.invoke({"text": text})
+        end_time = time.time()
+
+        self.memory.chat_memory.add_ai_message(response['text'])
+
+        elapsed_time = int((end_time - start_time) * 1000)
+        print(f"LLM ({elapsed_time}ms): {response['text']}")
+        return response['text']
