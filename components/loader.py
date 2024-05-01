b8ca213e202e4d6db28829656b20b7533ecc965c
achyutbenz19 <achyut.benz@gmail.com>
2024-05-01 16:06:52 -0500

    added splitters

--- components/loader.py
+++ components/loader.py
@@ -2,24 +2,34 @@ import json
 from pathlib import Path
 from pprint import pprint
 from langchain_community.document_loaders import JSONLoader, UnstructuredMarkdownLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
 
 class DocumentLoader:
     def __init__(self):
         return
     
-    def json(self, path):
+    def json(self, path, chunk_size=1000, chunk_overlap=0):
         loader = JSONLoader(
             file_path=path,
             jq_schema='.content',
             text_content=False
         )
         docs = loader.load()
-        return docs
+        splitted_docs = self.text_split(docs, chunk_size, chunk_overlap)
+        return splitted_text
     
-    def markdown(self, path):
+    def markdown(self, path, chunk_size=1000, chunk_overlap=0):
         loader = UnstructuredMarkdownLoader(markdown_path)
         docs = loader.load
-        return docs
+        splitted_docs = self.text_split(docs, chunk_size, chunk_overlap)
+        return splitted_text
+    
+    @staticmethod
+    def text_split(self, docs, chunk_size, chunk_overlap):
+        text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        texts = text_splitter.split_documents(documents=docs)
+        return texts
     
 # doc = DocumentLoader()
 # doc.json("data/buffets.json")
