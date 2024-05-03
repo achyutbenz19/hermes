30bd057608f5c7580e61a630cefa319e90a1edd0
Achyut Krishna Byanjankar <115382231+achyutbenz19@users.noreply.github.com>
2024-05-02 21:20:08 -0500

    Update twilio.py

--- components/twilio.py
+++ components/twilio.py
@@ -1,56 +1,71 @@
 import base64
 import json
 import logging
-from flask import Flask
+import base64
+import json
+import threading
+from components.stt import SpeechClientBridge
+from flask import Flask, render_template
 from flask_sockets import Sockets
+from google.cloud.speech import RecognitionConfig, StreamingRecognitionConfig
+
+HTTP_SERVER_PORT = 8000
+
+config = RecognitionConfig(
+    encoding=RecognitionConfig.AudioEncoding.MULAW,
+    sample_rate_hertz=8000,
+    language_code="en-US",
+)
+streaming_config = StreamingRecognitionConfig(config=config, interim_results=True)
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
-HTTP_SERVER_PORT = 8000
+def on_transcription_response(response):
+    if not response.results:
+        return
+
+    result = response.results[0]
+    if not result.alternatives:
+        return
+
+    transcription = result.alternatives[0].transcript
+    print("Transcription: " + transcription)
 
 @sockets.route('/connection', websocket=True)
-def echo(ws):
-    app.logger.info("Connection accepted")
-    # A lot of messages will be sent rapidly. We'll stop showing after the first one.
-    has_seen_media = False
-    message_count = 0
+def transcript(ws):
+    print("WS connection opened")
+    bridge = SpeechClientBridge(streaming_config, on_transcription_response)
+    t = threading.Thread(target=bridge.start)
+    t.start()
+
     while not ws.closed:
         message = ws.receive()
         if message is None:
-            app.logger.info("No message received...")
-            continue
-
-        # Messages are a JSON encoded string
-        data = json.loads(message)
-
-        # Using the event type you can determine what type of message you are receiving
-        if data['event'] == "connected":
-            app.logger.info("Connected Message received: {}".format(message))
-        if data['event'] == "start":
-            app.logger.info("Start Message received: {}".format(message))
-        if data['event'] == "media":
-            if not has_seen_media:
-                app.logger.info("Media message: {}".format(message))
-                payload = data['media']['payload']
-                app.logger.info("Payload is: {}".format(payload))
-                chunk = base64.b64decode(payload)
-                app.logger.info("That's {} bytes".format(len(chunk)))
-                app.logger.info("Additional media messages from WebSocket are being suppressed....")
-                has_seen_media = True
-        if data['event'] == "closed":
-            app.logger.info("Closed Message received: {}".format(message))
+            bridge.add_request(None)
+            bridge.terminate()
             break
-        message_count += 1
 
-    app.logger.info("Connection closed. Received a total of {} messages".format(message_count))
+        data = json.loads(message)
+        if data["event"] in ("connected", "start"):
+            print(f"Media WS: Received event '{data['event']}': {message}")
+            continue
+        if data["event"] == "media":
+            media = data["media"]
+            chunk = base64.b64decode(media["payload"])
+            bridge.add_request(chunk)
+        if data["event"] == "stop":
+            print(f"Media WS: Received event 'stop': {message}")
+            print("Stopping...")
+            break
 
+    bridge.terminate()
+    print("WS connection closed")
 
 if __name__ == '__main__':
     app.logger.setLevel(logging.DEBUG)
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
-
     server = pywsgi.WSGIServer(('', HTTP_SERVER_PORT), app, handler_class=WebSocketHandler)
     print("Server listening on: http://localhost:" + str(HTTP_SERVER_PORT))
     server.serve_forever()
