298bfe76f2551378d75e1225605c5f44b170bca2
achyutbenz19 <achyut.benz@gmail.com>
2024-05-07 09:44:57 -0500

    nodejs beta

--- components/tts.js
+++ components/tts.js
@@ -1,45 +1,53 @@
-const EventEmitter = require('events');
-const { Buffer } = require('node:buffer');
-const fetch = require('node-fetch');
+const EventEmitter = require("events");
+const { Buffer } = require("node:buffer");
+const fetch = require("node-fetch");
 
 class TextToSpeech extends EventEmitter {
-    constructor(config) {
-        super();
-        this.config = config;
-        this.config.voiceId ||= process.env.VOICE_ID;
-        this.nextExpectedIndex = 0;
-        this.speechBuffer = {};
+  constructor(config) {
+    super();
+    this.config = config;
+    this.config.voiceId ||= process.env.VOICE_ID;
+    this.nextExpectedIndex = 0;
+    this.speechBuffer = {};
+  }
+
+  async generate(gptReply, interactionCount) {
+    const { partialResponseIndex, partialResponse } = gptReply;
+
+    if (!partialResponse) {
+      return;
     }
 
-    async generate(gptReply, interactionCount) {
-        const { partialResponseIndex, partialResponse } = gptReply;
-
-        if (!partialResponse) { return; }
-
-        try {
-            const outputFormat = 'ulaw_8000';
-            const response = await fetch(
-                `https://api.elevenlabs.io/v1/text-to-speech/${this.config.voiceId}/stream?output_format=${outputFormat}&optimize_streaming_latency=3`,
-                {
-                    method: 'POST',
-                    headers: {
-                        'xi-api-key': process.env.XI_API_KEY,
-                        'Content-Type': 'application/json',
-                        accept: 'audio/wav',
-                    },
-                    body: JSON.stringify({
-                        model_id: process.env.XI_MODEL_ID,
-                        text: partialResponse,
-                    }),
-                }
-            );
-            const audioArrayBuffer = await response.arrayBuffer();
-            this.emit('speech', partialResponseIndex, Buffer.from(audioArrayBuffer).toString('base64'), partialResponse, interactionCount);
-        } catch (err) {
-            console.error('Error occurred in TextToSpeech service');
-            console.error(err);
-        }
+    try {
+      const outputFormat = "ulaw_8000";
+      const response = await fetch(
+        `https://api.elevenlabs.io/v1/text-to-speech/${this.config.voiceId}/stream?output_format=${outputFormat}&optimize_streaming_latency=3`,
+        {
+          method: "POST",
+          headers: {
+            "xi-api-key": process.env.XI_API_KEY,
+            "Content-Type": "application/json",
+            accept: "audio/wav",
+          },
+          body: JSON.stringify({
+            model_id: process.env.XI_MODEL_ID,
+            text: partialResponse,
+          }),
+        },
+      );
+      const audioArrayBuffer = await response.arrayBuffer();
+      this.emit(
+        "speech",
+        partialResponseIndex,
+        Buffer.from(audioArrayBuffer).toString("base64"),
+        partialResponse,
+        interactionCount,
+      );
+    } catch (err) {
+      console.error("Error occurred in TextToSpeech service");
+      console.error(err);
     }
+  }
 }
 
 module.exports = { TextToSpeech };
