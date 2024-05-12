bfaf7db64048696076e829f94ef01c4de834ca55
achyutbenz19 <achyut.benz@gmail.com>
2024-05-11 22:52:10 -0500

    new code

--- pages/api/connection.js
+++ pages/api/connection.js
@@ -3,47 +3,62 @@ import { Socket } from '@/components/socket'
 import { SpeechToText } from "@/components/stt";
 import { TextToSpeech } from "@/components/tts";
 import { VectorStore } from "@/components/vectorstore";
+import { Server as WebSocketServer } from 'ws';
 
 const SocketHandler = (req, res) => {
-    if (res.socket.server.io) {
+    if (res.socket.server.ws) {
         console.log('Socket is already running');
     } else {
         console.log('Socket is initializing');
-        const io = new Socket(res.socket.server);
+        const wss = new WebSocketServer({ server: res.socket.server });
 
-        io.on("connection", (socket) => {
-            console.log("Client connected");
+        wss.on('connection', (ws) => {
+            ws.on('error', console.error);
             let streamSid;
-            let callSid;
+
             const model = new LanguageModelProcessor();
+            const socket = new Socket(ws);
             const stt = new SpeechToText();
             const tts = new TextToSpeech({});
             const vectorstore = new VectorStore();
+
             let marks = [];
             let interactionCount = 0;
 
-            socket.on("message", async function message(data) {
+            ws.on('message', function message(data) {
                 const msg = JSON.parse(data);
-                if (msg.event === "start") {
+                if (msg.event === 'start') {
                     streamSid = msg.start.streamSid;
                     callSid = msg.start.callSid;
-                    console.log(
-                        `Twilio -> Starting Media Stream for ${streamSid}`.underline.red,
-                    );
-                } else if (msg.event === "media") {
+                    socket.setStreamSid(streamSid);
+                    console.log(`Twilio -> Starting Media Stream for ${streamSid}`.underline.red);
+                } else if (msg.event === 'media') {
                     stt.send(msg.media.payload);
-                } else if (msg.event === "mark") {
-                    console.log(
-                        `Twilio -> Audio completed mark (${msg.sequenceNumber}): ${msg.mark.name}`.red,
-                    );
-                    marks = marks.filter(m => m !== msg.mark.name);
-                } else if (msg.event === "stop") {
+                } else if (msg.event === 'mark') {
+                    const label = msg.mark.name;
+                    console.log(`Twilio -> Audio completed mark (${msg.sequenceNumber}): ${label}`.red);
+                    marks = marks.filter((m) => m !== msg.mark.name);
+                } else if (msg.event === 'stop') {
                     console.log(`Twilio -> Media stream ${streamSid} ended.`.underline.red);
                 }
             });
 
-            stt.on("transcription", async (text) => {
-                if (!text) return;
+            stt.on('utterance', async (text) => {
+                if (marks.length > 0 && text?.length > 5) {
+                    console.log('Twilio -> Interruption, Clearing stream'.red);
+                    ws.send(
+                        JSON.stringify({
+                            streamSid,
+                            event: 'clear',
+                        })
+                    );
+                }
+            });
+
+            stt.on('transcription', async (text) => {
+                if (!text) {
+                    return;
+                }
                 console.log(`Interaction ${interactionCount} – STT -> GPT: ${text}`.yellow);
                 const relevantDocs = await vectorstore.queryVectorStore(text);
                 const modelResponse = await model.chat(text, relevantDocs);
@@ -55,16 +70,19 @@ const SocketHandler = (req, res) => {
                 tts.generate(modelReply, interactionCount);
             });
 
-            tts.on("speech", (responseIndex, audio, label, icount) => {
+            tts.on('speech', (responseIndex, audio, label, icount) => {
                 console.log(`Interaction ${icount}: TTS -> TWILIO: ${label}`.blue);
-                socket.emit("audio", { responseIndex, audio, label });
+                socket.buffer(responseIndex, audio);
             });
 
-            socket.on("audiosent", (markLabel) => {
+            socket.on('audiosent', (markLabel) => {
                 marks.push(markLabel);
             });
         });
+
+        res.socket.server.ws = wss;
     }
+
     res.end();
 };
 
